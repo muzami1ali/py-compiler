8e3793592143c8bb5025f04f8710f6858413a77b
muzami1ali <muzamilalibosan@icloud.com>
2024-04-09 17:42:44 +0100

    Update compile/run

--- src/benchmarks/run.py
+++ src/benchmarks/run.py
@@ -15,7 +15,7 @@ import re
 
 name = (re.search("[a-zA-Z0-9_-]+", sys.argv[1])).group()
 pwd = os.getcwd()
-os.system(f"python ~/Work/PRJ/py-compiler/src/Driver.py {pwd}/{sys.argv[1]} {pwd}/{name}")
+os.system(f"python ../Driver.py {pwd}/{sys.argv[1]} {pwd}/{name}")
 
 os.system(f"clang {name}.ll -o {name}")
 os.system(f"./{name}")
