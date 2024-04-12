080ff70695caddc501096aaa85f4ccc32bb07772
muzami1ali <muzamilalibosan@icloud.com>
2024-04-12 15:51:31 +0100

    Update readme

--- src/benchmarks/compile.py
+++ src/benchmarks/compile.py
@@ -6,4 +6,4 @@ name = (re.search("[a-zA-Z0-9]+", sys.argv[1])).group()
 pwd = os.getcwd()
 os.system(f"python ../Driver.py {pwd}/{sys.argv[1]} {pwd}/{name}")
 
-os.system(f"clang {name}.ll -o {name}.o")
+os.system(f"clang {name}.ll -o {name}")
